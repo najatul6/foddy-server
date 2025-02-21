476beb402d53acceb8977906b941d59e1439b65e
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-21 23:09:56 +0600

    Add JSON body parsing middleware to Express server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 5000 || process.env.PORT
-
+app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Foddy Server is running...')
 })
