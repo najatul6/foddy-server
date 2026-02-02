71cbf7afe4032dc779537f7792d7633973c0781e
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-02-03 02:08:04 +0600

    Add JWT library for token handling

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const jwt = require("jsonwebtoken");
 const port = 5000 || process.env.PORT
 app.use(express.json())
 app.get('/', (req, res) => {
